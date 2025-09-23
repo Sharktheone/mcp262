3847951305fe9b67c9fec672c691ea42d3dc87e8
Shark <sharktheone@proton.me>
2025-09-23 09:39:01 +0200

    load ci res from github

--- runner/ci/diff.go
+++ runner/ci/diff.go
@@ -2,10 +2,13 @@ package ci
 
 import (
 	"encoding/json"
+	"net/http"
 	"os"
 
 	"github.com/Sharktheone/mcp262/runner/results"
 )
+
+const CI_GITHUB_URL = "https://raw.githubusercontent.com/Sharktheone/yavashark-data/main/results.json"
 
 func LoadPrevCi(path string) (*results.TestResults, error) {
 	contents, err := os.ReadFile(path)
@@ -27,4 +30,28 @@ func LoadPrevCi(path string) (*results.TestResults, error) {
 	res := results.ConvertResultsFromCI(resultsCI)
 
 	return results.FromResults(res), nil
+}
+
+func LoadPrevCiGithub() (*results.TestResults, error) {
+	res, err := http.Get(CI_GITHUB_URL)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return nil, err
+	}
+
+	var resultsCI []results.CIResult
+	err = json.NewDecoder(res.Body).Decode(&resultsCI)
+
+	if err != nil {
+		return nil, err
+	}
+
+	r := results.ConvertResultsFromCI(resultsCI)
+
+	return results.FromResults(r), nil
 }
